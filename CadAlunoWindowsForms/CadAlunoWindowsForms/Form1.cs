70600beacc6de1aa49da552edb2db0aaa3a9059d
DANIEL CATANEO <081240030@faculdade.cefsa.edu.br>
2026-02-10 20:44:44 -0300

    Projeto Aluno SQL

---
+++ CadAlunoWindowsForms/CadAlunoWindowsForms/Form1.cs
@@ -0,0 +1,45 @@
+﻿using CadAlunoWindowsForms.DAO.DAO;
+using CadAlunoWindowsForms.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadAlunoWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AlunoViewModel aluno = new AlunoViewModel();
+                aluno.Id = Convert.ToInt32(txtID.Text);
+                aluno.Nome = txtNome.Text;
+                aluno.Mensalidade = Convert.ToDouble(txtMensalidade.Text);
+                aluno.DataNascimento = Convert.ToDateTime(TxtNascimento.Text);
+                aluno.CidadeId = Convert.ToInt32(txtCidade.Text);
+
+                AlunoDAO dao = new AlunoDAO();
+                dao.Inserir(aluno);
+
+            }
+            catch (Exception erro)
+            {
+               MessageBox.Show(erro.Message);     
+            }
+
+        }
+    }
+}
---
+++ SQL-Scripts/Cria-Tabela-Aluno.sql
@@ -0,0 +1,12 @@
+USE AulaDB
+GO
+
+CREATE TABLE Alunos ( 
+ Id int NOT NULL PRIMARY KEY,
+ nome varchar(50) NULL, 
+ mensalidade decimal (18, 2) NULL, 
+ cidadeId int NULL, 
+ DataNascimento datetime NULL 
+)
+
+
