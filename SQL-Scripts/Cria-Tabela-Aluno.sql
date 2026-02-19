a90227ff6eacbf77217a8da93991a1f915dab234
DANIEL CATANEO <081240030@faculdade.cefsa.edu.br>
2026-02-18 22:26:48 -0300

    Create DB

--- SQL-Scripts/Cria-Tabela-Aluno.sql
+++ SQL-Scripts/Cria-Tabela-Aluno.sql
@@ -9,13 +9,4 @@ CREATE TABLE Alunos(
  DataNascimento datetime NULL 
 )
 
-insert into Alunos values (1,'Daniel',500.00,1,'2007-01-21')
-
-delete Alunos where id = 1 
-
-SELECT * FROM Alunos
-
-UPDATE Alunos
-SET nome = 'oi'
-where id = 1
 
